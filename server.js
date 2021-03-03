c6896a57f0b85d026ea662e00b15135f566c2e83
Cody Kimb <codykimbell@gmail.com>
2021-03-03 16:12:32 -0600

    create route

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
 
+const { notes } = require('./Develop/db/db.json');
+
+app.get('/api/notes', (req, res) => {
+    res.json(notes);
+  });
+
 app.listen(3001, () => {
     console.log(`API server now on port 3001!`);
   });
