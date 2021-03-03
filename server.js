6e56534b3f5f1ad8aacedcebe8a4aacc05d3c1de
Cody Kimb <codykimbell@gmail.com>
2021-03-03 16:37:52 -0600

    add heroku

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const PORT = process.env.PORT || 3001;
+
 const express = require('express');
 const app = express();
 
@@ -7,6 +9,6 @@ app.get('/api/notes', (req, res) => {
     res.json(notes);
   });
 
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
+  app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
   });
