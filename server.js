2e6a23d58a8a7bd0f55504b5b1139ef917aee961
Cody Kimb <codykimbell@gmail.com>
2021-03-03 18:54:50 -0600

    POST data route

--- server.js
+++ server.js
@@ -3,12 +3,24 @@ const PORT = process.env.PORT || 3001;
 const express = require('express');
 const app = express();
 
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+
+// parse incoming JSON data
+app.use(express.json());
+
 const { notes } = require('./db/db.json');
 
 app.get('/api/notes', (req, res) => {
     res.json(notes);
   });
 
-  app.listen(PORT, () => {
+app.post('/api/notes', (req, res) => {
+    console.log(req.body);
+    res.json(req.body);
+});
+
+
+app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
   });
