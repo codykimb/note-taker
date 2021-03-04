020f85050cdc877faa4b052c4d2bf13944eac53f
Cody Kimb <codykimbell@gmail.com>
2021-03-03 18:29:12 -0600

    got rid of Develop folder and extra package.json file

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const PORT = process.env.PORT || 3001;
 const express = require('express');
 const app = express();
 
-const { notes } = require('./Develop/db/db.json');
+const { notes } = require('./db/db.json');
 
 app.get('/api/notes', (req, res) => {
     res.json(notes);
